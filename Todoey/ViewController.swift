0f90608904091dc1e8a80b39839b4d0c80498c70
Dustin Prevatt <dustinprevatt@dustins-macbook-pro.local>
2018-12-08 00:49:05 -0600

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Dustin Prevatt on 12/8/18.
+//  Copyright © 2018 Dustin Prevatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
