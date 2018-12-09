4bc92dca458af66e72701b55314407293da73b9e
Dustin Prevatt <dustinprevatt@dustins-macbook-pro.local>
2018-12-08 22:32:24 -0600

    Added TableView Methods for UITableView Delegate and Datasource with sample data

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
+    let itemArray = ["Find Mike", "Buy Eggos", "Destory Demogorgon"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //MARK: TableView Datascource Methods
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell;
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    //MARK: TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        // Add a checkmark to the cell when it is selected
+        if tableView.cellForRow(at: indexPath)?.accessoryType.rawValue == 3 {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
 }
 
